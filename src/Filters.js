f0e70efd8b7e7363f83963be6df392371d9623fa
Brittany Miller <bmiller@unow.edu>
2019-09-28 10:35:07 -0500

    Whitespace changes for linter

--- src/Filters.js
+++ src/Filters.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Filters = () => {
-	return (
-	  <section id="filter-div">
-	    <div className="offset">
-	      <h3>Filter projects by skill</h3>
-	      <hr />
-	      <ul id="filter">
-	        <li><button href="#" className="current">Show All</button></li>
-	        <li><button href="#">Arts</button></li>
-	        <li><button href="#">Games & Puzzles</button></li>
-	        <li><button href="#">Graphics & Layout</button></li>
-	        <li><button href="#">Tech</button></li>
-	      </ul>
-	    </div>
-	  </section>
-	);
+  return (
+    <section id="filter-div">
+      <div className="offset">
+        <h3>Filter projects by skill</h3>
+        <hr />
+        <ul id="filter">
+          <li><button href="#" className="current">Show All</button></li>
+          <li><button href="#">Arts</button></li>
+          <li><button href="#">Games & Puzzles</button></li>
+          <li><button href="#">Graphics & Layout</button></li>
+          <li><button href="#">Tech</button></li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Filters;
