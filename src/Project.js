b1c8d6d81a095ac5903a67af34dbed4fd1f8e7c4
Brittany Miller <bmiller@unow.edu>
2019-09-27 20:16:01 -0500

    Add conditional rendering based on 'active' flag

--- src/Project.js
+++ src/Project.js
@@ -1,25 +1,22 @@
 import React from 'react';
 
 
-const Project = ({id, classes, thumblink, thumbsrc, thumbalt, thumbtitle, namelink, name, lightbox, descr, tech, active }) => {
-  // if ({active}) {
-  	return (
-      <div id={id} className={`item displayed bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5 ${classes} `}>
-        <div className="offset">
-          <a href={thumblink} rel="noreferrer noopener lightbox">
-            <img src={`./images/${thumbsrc}`} alt={thumbalt} title={thumbtitle} width="200px" height="200px" />
-          </a>
-          <div className="caption">
-            <a href={namelink} rel="lightbox noreferrer nooopener">{name}</a>
-            <p className="descrip">{descr}</p>
-            <ul className="technologies">{tech}
-            </ul>
-          </div>
+const Project = ({images, id, classes, thumblink, thumbsrc, thumbalt, thumbtitle, namelink, name, lightbox, descr, tech, active }) => {
+	return (
+    <div id={id} className={`item displayed bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5 ${classes} `}>
+      <div className="offset">
+        <a href={thumblink} rel="noreferrer noopener lightbox">
+          <img src={`./images/${thumbsrc}`} alt={thumbalt} title={thumbtitle} width="200px" height="200px" />
+        </a>
+        <div className="caption">
+          <a href={namelink} rel="lightbox noreferrer nooopener">{name}</a>
+          <p className="descrip">{descr}</p>
+          <ul className="technologies">{tech}
+          </ul>
         </div>
       </div>
-  	);
-  // }
-  // return null;
+    </div>
+	);
 }
 
 export default Project;
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -5,23 +5,27 @@ import Project from './Project';
 const ProjectList = ({projects}) => {
 	
 	const projectComponent = projects.map((record, i) => {
-		return ( 
-			<Project 
-				key={i} 
-				id={projects[i].id} 
-				classes={projects[i].classes} 
-				thumblink={projects[i].thumblink}
-				thumbsrc={projects[i].thumbsrc} 
-				thumbalt={projects[i].thumbalt} 
-				thumbtitle={projects[i].thumbtitle} 
-				namelink={projects[i].namelink}
-				name={projects[i].name} 
-				lightbox={projects[i].lightbox}
-				descr={projects[i].descr} 
-				tech={projects[i].tech}
-				active={projects[i].active}
-			/>
-		)
+		if (record.active) {
+			return ( 
+				<Project
+					images={images}
+					key={i}
+					id={projects[i].id}
+					classes={projects[i].classes}
+					thumblink={projects[i].thumblink}
+					thumbsrc={projects[i].thumbsrc}
+					thumbalt={projects[i].thumbalt}
+					thumbtitle={projects[i].thumbtitle}
+					namelink={projects[i].namelink}
+					name={projects[i].name}
+					lightbox={projects[i].lightbox}
+					descr={projects[i].descr}
+					tech={projects[i].tech}
+					active={projects[i].active}
+				/>
+			);
+		}
+		return null;
 	})
 	return (
 	  <section id="portfolio">
